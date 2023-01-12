afc6027b6b05c10443cd8f5ded067f076cbd2679
biaoli <biaoli6@hotmail.com>
2023-01-12 09:53:52 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+import { Component } from '@angular/core';  
+import studentsData from './students.json';  
+    
+interface Student {  
+    id: Number;  
+    name: String;  
+    email: String;  
+    gender: String;  
+}  
+    
+@Component({  
+  selector: 'my-app',  
+  templateUrl: './app.component.html',  
+  styleUrls: [ './app.component.css' ]  
+})  
+export class AppComponent  {  
+  name = 'Angular';  
+    
+  students: Student[] = studentsData;  
+}  
