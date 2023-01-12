20cfc305bb74f919adfbff359018f5e53c30276b
biaoli <biaoli6@hotmail.com>
2023-01-12 11:57:45 -0500

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';  
-import studentsData from './students.json';  
-    
-interface Student {  
-    id: Number;  
-    name: String;  
-    email: String;  
-    gender: String;  
-}  
     
 @Component({  
   selector: 'my-app',  
   templateUrl: './app.component.html',  
   styleUrls: [ './app.component.css' ]  
 })  
-export class AppComponent  {      
-  jsonData: Student[] = studentsData;  
+export class AppComponent  { 
 }  
